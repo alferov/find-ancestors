32e849d00674e2f6b119709ff9d10d166a87f8ff
Philipp Alferov <philipp.alferov@gmail.com>
2015-10-04 18:48:50 -0400

    Refactoring

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
-
 var extend = require('shallow-object-extend');
 var isArray = require('isarray');
 var isObject = require('isobject');
 
+// Generate unique ID
 var generateUniqueNumber = function() {
   var unique = 0;
 
@@ -19,12 +19,24 @@ var generateUniqueNumber = function() {
 
 var generator = generateUniqueNumber();
 
-var traverse = function(object, childrenProperty, predicate, parent) {
+// Recursive tree traversal
+var traverse = function(options) {
   var result = null;
+  var object = options.object;
+  var childrenProperty = options.childrenProperty;
+  var predicate = options.predicate;
+  var modifier = options.modifier;
+  var parent = options.parent;
 
   if (isArray(object)) {
     for (var i = 0; i < object.length; i++) {
-      result = traverse(object[i], childrenProperty, predicate, parent);
+      result = traverse({
+        object: object[i],
+        childrenProperty: childrenProperty,
+        predicate: predicate,
+        modifier: modifier,
+        parent: parent
+      });
 
       if (result) {
         break ;
@@ -33,10 +45,9 @@ var traverse = function(object, childrenProperty, predicate, parent) {
   }
 
   if (isObject(object)) {
-    object.__id = generator.next();
 
-    if (parent) {
-      object.__parent = parent;
+    if (modifier) {
+      object = modifier(object, parent);
     }
 
     if (predicate(object)) {
@@ -44,12 +55,30 @@ var traverse = function(object, childrenProperty, predicate, parent) {
     }
 
     if (object[childrenProperty]) {
-      result = traverse(object[childrenProperty], childrenProperty, predicate, object.__id);
+      result = traverse({
+        object: object[childrenProperty],
+        childrenProperty: childrenProperty,
+        predicate: predicate,
+        modifier: modifier,
+        parent: object.__id
+      });
+
     }
   }
 
   return result;
 };
+
+var modifier = function(object, parent) {
+  object.__id = generator.next();
+
+  if (parent) {
+    object.__parent = parent;
+  }
+
+  return object;
+};
+
 /**
  * ancestors
  *
@@ -61,14 +90,22 @@ var traverse = function(object, childrenProperty, predicate, parent) {
 module.exports = function ancestors(options) {
   options = extend({
     childrenProperty: 'children',
-    identifier: 'id'
+    identifier: 'id',
+    // Callback that takes every node and its parent (if node is nested)
+    // as arguments. It should return a node.
+    modifier: modifier
   }, options);
-
-  generator.empty();
 
   if (!isArray(options.data)) {
     throw new TypeError('Expected array');
   }
 
-  return traverse(options.data, options.childrenProperty, options.predicate);
+  generator.empty();
+
+  return traverse({
+    object:options.data,
+    childrenProperty: options.childrenProperty,
+    predicate: options.predicate,
+    modifier: options.modifier
+  });
 };
--- test/fixtures/expected.js
+++ test/fixtures/expected.js
@@ -2,15 +2,19 @@ module.exports = [{
   id: 1,
   children: [{
     id: 2,
+    __parent: 1,
     children: [{
-      id: 3
+      id: 3,
+      __parent: 2
     }]
   }]
 }, {
   id: 2,
   children: [{
-    id: 3
+    id: 3,
+    __parent: 2
   }]
 }, {
-  id: 3
+  id: 3,
+  __parent: 2
 }];
